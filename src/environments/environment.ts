9480ed955156e8ec7d9f2582be8c5de2b6697fe6
IpCeSs <ceciliapigeolet@gmail.com>
2018-04-12 14:07:46 +0200

    ajout d'un utilisateur et authentification via firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD5P4ZQxvEmK0wmRlt-5UCmKE2CgmCNp-k",
+    authDomain: "real-473b8.firebaseapp.com",
+    databaseURL: "https://real-473b8.firebaseio.com",
+    projectId: "real-473b8",
+    storageBucket: "real-473b8.appspot.com",
+    messagingSenderId: "740180753560"
+
+}
+};
