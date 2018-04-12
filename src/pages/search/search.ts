f78ffbb0c3465ef9f10913ed720595fe02159ae3
Eric LE DEVEDEC <ledevedec.eric@gmail.com>
2018-04-12 14:48:53 +0200

    Ajout des Fonctions sur la page recherche

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {exitCodeFromResult} from "@angular/compiler-cli";
+import {ProfilPage} from "../profil/profil";
 
 /**
  * Generated class for the SearchPage page.
@@ -15,11 +17,83 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
+  users: any[] = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.initialUsers();
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+  }
+
+
+  private initialUsers() {
+    this.users = [
+      {
+        id: 1,
+        prenom: 'Eric',
+        nom: 'Le devedec',
+        age: '25',
+        avatar: 'http://i.pravatar.cc/150?img=1'
+      },
+      {
+        id: 2,
+        prenom: 'Cess',
+        nom: 'Dupont',
+        age: '25',
+        avatar: 'http://i.pravatar.cc/150?img=2'
+      },
+      {
+        id: 3,
+        prenom: 'Jean',
+        nom: 'Chales',
+        age: '25',
+        avatar: 'http://i.pravatar.cc/150?img=3'
+      }
+    ];
+    /*    this.users = this.users.map((user) => {
+
+          let prenom = user.prenom;
+
+          return {id : user.id ,prenom : prenom.charAt(0).toUpperCase() + prenom.slice(1)};
+        }) */
+
+
+  }
+
+
+  public getUser(query: any) {
+
+    let val = query.target.value;
+
+    this.initialUsers();
+
+
+    if (val && val.trim() != '') {
+      this.users = this.users.filter((user) => {
+
+
+        return (user.prenom.toLowerCase().indexOf(val.toLowerCase()) > -1);
+
+
+      })
+    }
+  }
+
+
+  public addContact(user) {
+    // Eric@TODO à Finir
+
+    console.log(`Ok Contact : ${user.prenom}  avec l' ID : ${user.id} a été ajouté`);
+  }
+
+
+  public goToUserProfil(user) {
+
+    this.navCtrl.push(ProfilPage, user)
   }
 
 }
