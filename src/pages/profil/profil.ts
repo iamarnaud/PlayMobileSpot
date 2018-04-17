dc8e2cb52856356e27e19eaa6c777fd396ffd21c
Arnaud <arnaud.voiron@gmail.com>
2018-04-17 13:16:43 +0200

    page profil : ajout css et carte géoloc

--- src/pages/profil/profil.ts
+++ src/pages/profil/profil.ts
@@ -1,32 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NavController, AlertController, NavParams} from 'ionic-angular';
 import {LoginPage} from '../login/login';
 import {ItemsProvider} from "../../providers/items/items";
 import {AuthProvider} from "../../providers/auth/auth";
+import {Geolocation} from "@ionic-native/geolocation";
+
+declare var google;
 
 @Component({
   selector: 'page-profil',
   templateUrl: 'profil.html'
 })
+
 export class ProfilPage {
 
   UID ;
   user: object;
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(
     public navCtrl: NavController,
     public alertCtrl: AlertController,
     public navParams: NavParams,
     public userService: ItemsProvider,
-    public userAuth: AuthProvider
+    public userAuth: AuthProvider,
+    public geolocation: Geolocation
   ) {
-
     this.getUserAuthIUD();
 
     if (typeof(navParams.get('UID')) !== 'undefined') {
       this.UID = navParams.get('UID')
-    };
+    }
+    ;
 
     this.getUsers();
 
@@ -56,7 +63,7 @@ export class ProfilPage {
   getUsers() {
     this.userService.getUsers().subscribe(data => {
 
-     this.user =  data.filter( elem => {
+      this.user = data.filter(elem => {
         return elem.UID === this.UID;
       });
       // Transforme le tebleau en objet
@@ -70,4 +77,24 @@ export class ProfilPage {
   }
 
 
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    this.geolocation.getCurrentPosition().then((position) => {
+
+    let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }, (err) => {
+      console.log(err);
+    });
+  }
 }
