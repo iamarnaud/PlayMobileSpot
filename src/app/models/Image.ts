ab735fe89a576b74b9a8ec42880b1cd0060f10ba
Alexandre <a.biagi@orange.fr>
2018-04-18 15:15:31 +0200

    Page Flux

--- src/app/models/Image.ts
+++ src/app/models/Image.ts
@@ -2,11 +2,11 @@ import { User } from '../../app/models/User';
 import {Timestamp} from "rxjs/Rx";
 
 export class Image extends User {
-  id?: string;
+/*  id?: string;
   lien?: string;
   likeCount?: number;
   date?: Timestamp<any>;
   com_count?:number;
-  commentaires?: object;
+  commentaires?: object;*/
 
 }
--- src/providers/images/images.ts
+++ src/providers/images/images.ts
@@ -1,6 +1,6 @@
 
-import { Injectable } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from "angularfire2/firestore";
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, QueryFn} from "angularfire2/firestore";
 import { Image } from '../../app/models/Image';
 import {Observable} from "rxjs/Observable";
 
@@ -18,12 +18,22 @@ export class ImagesProvider {
   images: Observable<Image[]>
 
   constructor(public afs:AngularFirestore) {
-    this.images = this.afs.collection('images').valueChanges();
+    this.images = this.afs.collection('posts').valueChanges();
+    this.imagesCollection = this.afs.collection('posts');
   }
 
-  getImages(){
-    this.images = this.afs.collection('images').valueChanges();
-    return this.images;
+  getImagesDesc(){
+   return this.afs.collection('posts', (data) => data.orderBy('timestamp', "desc").limit(50) ).valueChanges();
+  }
+
+  addImage(post) {
+      this.imagesCollection.add(post)
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
   }
 
 }
